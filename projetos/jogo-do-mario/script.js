e7617ad58f674a27e5a57818b8a418550ab04579
Dryelle Ebelin <dryellesilva07@gmail.com>
2023-02-04 22:01:27 -0300

    update

---
+++ projetos/jogo-do-mario/script.js
@@ -0,0 +1,27 @@
+const mario = document.querySelector('.mario');
+const tubo = document.querySelector('.tubo');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    const tuboPosition = tubo.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    if (tuboPosition < 120 && tuboPosition > 0 && marioPosition < 80) {
+        tubo.style.animation = 'none';
+        tubo.style.left = `${tuboPosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = './img/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+    }
+}, 10); 
+
+document.addEventListener('keydown', jump);
